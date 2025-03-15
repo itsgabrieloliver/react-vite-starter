7d0587e04632d9150d1cda15c2b2dd6b218c3611
Gabriel Oliver <gabrielo@withnubo.com>
2025-03-14 23:46:34 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div class="flex flex-col items-center justify-center gap-4 w-full h-screen bg-neutral-950">
+        <h1 class="text-4xl font-bold text-neutral-50">
+          React + Vite + Tailwind
+        </h1>
+        <p class="font-medium text-neutral-400">Nubo Starter</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
